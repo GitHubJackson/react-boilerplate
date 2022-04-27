cd7b7333b38162a918fd6a8f90736a02ac0df8cc
zhouweibin <zhouweibin@senseauto.com>
2022-04-27 19:56:24 +0800

    feat: add package script

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,26 @@ import './App.css';
 function App() {
   const FallbackComponent = null;
   return (
-    <Router>
-      <Suspense fallback={FallbackComponent}>
-        <Routes>
-          {routers.length > 0 &&
-            routers.map((router) => {
-              return (
-                <Route
-                  path={router.path}
-                  element={<router.component />}
-                  key={router.path}
-                />
-              );
-            })}
-          {/* 路由重定向 */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <div className="app">
+      <Router>
+        <Suspense fallback={FallbackComponent}>
+          <Routes>
+            {routers.length > 0 &&
+              routers.map((router) => {
+                return (
+                  <Route
+                    path={router.path}
+                    element={<router.component />}
+                    key={router.path}
+                  />
+                );
+              })}
+            {/* 路由重定向 */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </div>
   );
 }
 
